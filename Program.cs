b8df477ce7631df0f7d5dcb2f89d6dfe4b1b25dd
Steve Dunn <steve@dunnhq.com>
2021-09-22 17:28:11 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CaBug
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    public class Foo
+    {
+        public int[] Numbers { get; set; } = new int[] { 1, 2, 3 };
+    }
+}
+
